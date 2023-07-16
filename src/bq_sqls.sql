57cecc76d0391d51a987a0e159ebba6caa5c1f26
swap83k <swap83k@gmail.com>
2023-07-16 16:31:33 +0530

    Update bq_sqls.sql

--- src/bq_sqls.sql
+++ src/bq_sqls.sql
@@ -2,7 +2,7 @@ create or replace view `aml_schema.aml_training_view`
 as select customer_id,country_of_residence,country_of_birth,trasnaction_amount,transaction_country,
 transaction_category,transaction_country_category,transaction_origin_currency,transaction_date,
 transaction_type,transaction_business,average_monthly_transaction,alert_flag
-from `fluent-night-392210.aml_schema.aml_input_train`
+from `<project_name>.aml_schema.aml_input_train`
 
  CREATE OR REPLACE MODEL
 				  `aml_schema.predict_transactions`
